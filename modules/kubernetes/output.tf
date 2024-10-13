1740df532c6247d56d9a5009313febe950761c1d
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-13 19:51:05 -0300

    feat: adds new outputs

---
+++ modules/kubernetes/output.tf
@@ -0,0 +1,9 @@
+output "app_service_load_balancer_ip" {
+  value = kubernetes_service_v1.app_service.spec[0].load_balancer_ip
+  description = "The external IP of the service"
+}
+
+output "app_service_port" {
+  value = kubernetes_service_v1.app_service.spec[0].port
+  description = "The port of the service"
+}
---
+++ output.tf
@@ -0,0 +1,9 @@
+output "app_service_load_balancer_ip" {
+  value = module.kubernetes.app_service_load_balancer_ip
+  description = "The external IP of the service"
+}
+
+output "app_service_port" {
+  value = module.kubernetes.app_service_port
+  description = "The port of the service"
+}
