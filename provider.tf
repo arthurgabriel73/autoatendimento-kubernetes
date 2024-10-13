70f7b236f825e643377aa10bdedbbe57e086a83f
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-13 19:38:59 -0300

    fix: fixes module reference

--- provider.tf
+++ provider.tf
@@ -34,7 +34,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks-cluster.certificate_authority)
+    cluster_ca_certificate = base64decode(module.eks.certificate_authority)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
