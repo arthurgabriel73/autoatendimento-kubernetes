c37bae91124515265ca33bdfaef2a5f6e768146d
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-13 19:36:51 -0300

    refactor: changes variabels name

--- provider.tf
+++ provider.tf
@@ -23,7 +23,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.certificate_authority)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
