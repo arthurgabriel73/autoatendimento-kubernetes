7d42a62088ece21423b09148a28d39b68c5a414d
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-14 11:53:59 -0300

    fix: removes unused helm

--- provider.tf
+++ provider.tf
@@ -30,15 +30,3 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.certificate_authority)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
-  }
-}
