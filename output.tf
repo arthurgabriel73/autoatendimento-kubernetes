3cd34631a6c8e8b689240f619ae68e4ffbdbdb07
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-16 00:00:31 -0300

    refactor: removes outputs

--- output.tf
+++ output.tf
@@ -2,13 +2,3 @@ output "app_service_load_balancer_ip" {
   value       = "http://${module.kubernetes.app_service_load_balancer.status[0].load_balancer[0].ingress[0].hostname}:${module.kubernetes.app_service_port[0].target_port}"
   description = "The host of the service"
 }
-
-output "app_service_port" {
-  value       = module.kubernetes.app_service_port[0].target_port
-  description = "The port of the service"
-}
-
-output "database_endpoint" {
-  value       = split(":", "${module.database.database_endpoint}")[0]
-  description = "The endpoint of the database"
-}
